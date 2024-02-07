9ef04c4fdbfe2506bbe9cc4ec379d3bd2c5475eb
Facundo Gaidano <41210748+facundogaidano@users.noreply.github.com>
2024-02-07 17:49:15 -0300

    Up to 3.21

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -18,6 +18,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id ' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -38,7 +40,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (body.name === undefined) {
@@ -55,6 +57,7 @@ app.post('/api/persons', (request, response) => {
   book.save().then(savedToPhonebook => {
     response.json(savedToPhonebook)
   })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -85,14 +88,13 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Phonebook.findByIdAndUpdate(request.params.id, person, { new: true })
+  Phonebook.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
